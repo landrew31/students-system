1766edcf18a3a84241ca488847f56e54391f4512
Andrew Lupa <lupa_andriy@list.ru>
2016-01-31 18:48:17 +0200

    added dynamic groups.html

--- students/views.py
+++ students/views.py
@@ -37,7 +37,18 @@ def students_delete(request, sid):
 #Views for Groups
 
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+	groups = (
+		{'id': 1,
+		 'name': 'KA-31',
+		 'leader': u'Лупа Андрій'},
+		{'id': 2,
+		 'name': 'KA-32',
+		 'leader': u'Круть Валерія'},
+		{'id': 3,
+		 'name': 'KA-33',
+		 'leader': u'Войтенко Анна'},
+		)
+	return render(request, 'students/groups_list.html', {'groups':groups})
 
 def groups_add(request):
 	return HttpResponse('<h1>Group Add Form</h1>')
