0de7a88e9a696a8c6b6e4158587f47a1d4e2e978
landrew31 <lupa_andriy@list.ru>
2016-05-06 12:32:31 +0300

    added Student model logging

--- students/apps.py
+++ students/apps.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.apps import AppConfig
 
 
-class StudentsConfig(AppConfig):
+class StudentsAppConfig(AppConfig):
     name = 'students'
+    verbose_name = u'База студентів'
+
+    def ready(self):
+    	from students import signals
+
+
